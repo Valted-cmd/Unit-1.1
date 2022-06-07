a1d2eb5016fb9202d7322be4d0c0e64d9cb1367a
Adavletbaev <alex.davletbaev@yandex.ru>
2022-06-08 01:39:20 +0300

    junit4

--- src/test/java/ru/netology/service/CashHackTest.java
+++ src/test/java/ru/netology/service/CashHackTest.java
@@ -1,32 +1,31 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 
 public class CashHackTest {
+    CashHack service = new CashHack();
 
-        CashHack service = new CashHack();
-
-        @Test
-        public void shouldAmountLessThan1000() {
-            int actual = service.remain(900);
-            int expected = 100;
-            assertEquals(actual, expected);
-        }
-
-        @Test
-        public void shouldAmountMoreThan1000() {
-            int actual = service.remain(1100);
-            int expected = 900;
-            assertEquals(actual, expected);
-        }
-
-        @Test
-        public void shouldAmount1000() {
-            int actual = service.remain(1000);
-            int expected = 0;
-            assertEquals(actual, expected);
-        }
+    @Test
+    public void shouldAmountLessThan1000() {
+        int actual = service.remain(900);
+        int expected = 100;
+        assertEquals(expected, actual);
     }
+
+    @Test
+    public void shouldAmountMoreThan1000() {
+        int actual = service.remain(1100);
+        int expected = 900;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAmount1000() {
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+}
